778ccc25122e09f7d921eac7390fc76550775a8f
Charlie Xia <lxia.usc@gmail.com>
2016-05-03 17:30:36 -0700

    tmp

--- test/swan_test.sh
+++ test/swan_test.sh
@@ -3,6 +3,5 @@
 #automated script to run SWAN test
 #the log of tests will be in all.log
 
-if [ ! -e "swan_test.tgz" ]; then wget http://meta.usc.edu/softs/swan/swan_test.tgz; fi
-tar -zxvf swan_test.tgz
+if [ ! -e "swan_test.tgz" ]; then wget http://meta.usc.edu/softs/swan/swan_test.tgz; tar -zxvf swan_test.tgz; fi
 ($SWAN_BIN/paired.sh all && $SWAN_BIN/single.sh all) >all.log 2>&1
