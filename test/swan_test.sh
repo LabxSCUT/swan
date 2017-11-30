1e7d1c4d1abe3c30abb556f7a8535318f3e5719e
Charlie Xia <lxia.usc@gmail.com>
2017-11-30 14:36:48 -0800

    fixed error relating to base::as.vector

--- test/swan_test.sh
+++ test/swan_test.sh
@@ -3,6 +3,6 @@
 #automated script to run SWAN test
 #the log of tests will be in all.log
 
-if [ ! -e "swan_test.tgz" ]; then wget http://meta.usc.edu/softs/swan/swan_test.tgz; fi
+if [ ! -e "swan_test.tgz" ]; then wget https://s3-us-west-2.amazonaws.com/lixiabucket/swan_test.tgz; fi
 tar -zxvf swan_test.tgz
 ($SWAN_BIN/paired.sh all && $SWAN_BIN/single.sh all) >all.log 2>&1
--- test/updateAWS.sh
+++ test/updateAWS.sh
@@ -1,1 +1,3 @@
-aws s3 cp swan_test.tgz s3://lixiabucket/swan_test.tgz --grants read=urihttp://acs.amazonaws.com/groups/global/AllUsers
+aws s3 cp swan_test.tgz https://s3-us-west-2.amazonaws.com/lixiabucket/swan_test.tgz --grants read=urihttp://acs.amazonaws.com/groups/global/AllUsers
+
+#https://s3-us-west-2.amazonaws.com/lixiabucket/swan_test.tgz
